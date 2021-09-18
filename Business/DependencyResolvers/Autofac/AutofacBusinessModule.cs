2e9ca4c0d582d7e97488e8f02cb533f36ece8e79
Sercan Sever <sercan.sever16@gmail.com>
2021-09-18 16:37:27 +0300

    DI Add

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EfContactManager>().As<IContactService>();
+            builder.RegisterType<EfContactDal>().As<IContactDal>();
+
+            builder.RegisterType<EfPhoneManager>().As<IPhoneService>();
+            builder.RegisterType<EfContactDal>().As<IContactDal>();
+
+            builder.RegisterType<EfLocationManager>().As<ILocationService>();
+            builder.RegisterType<EfContactDal>().As<IContactDal>();
+
+            builder.RegisterType<EfContactManager>().As<IContactService>();
+            builder.RegisterType<EfEmailDal>().As<IEmailDal>();
+        }
+    }
+}
